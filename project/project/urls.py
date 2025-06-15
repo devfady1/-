6de4afb1ff73c5845d201a3162eaf111d9f89086
FADY ASHRAF <fadya5323@gmail.com>
2025-06-15 08:06:07 +0300

    Update urls.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import handle404, handle500 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
